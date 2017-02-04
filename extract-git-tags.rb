3184e2ebf4b5bf28738a8609ca4a477d39024275
David Poetzsch-Heffter <davidpoetzsch@web.de>
2017-02-04 13:31:42 +0100

    supporting multiple repos

--- extract-git-tags.rb
+++ extract-git-tags.rb
@@ -3,19 +3,39 @@
 require "rugged"
 
 if ARGV.length < 1
-    puts "Usage: extract-git-tags.rb <git-repo>"
+    puts "Usage: extract-git-tags.rb <git-repos>..."
     exit 1
 end
 
-repo = Rugged::Repository.new(ARGV[0])
-
-repo.tags.sort_by{|t|t.name}.reverse.each do |tag|
-    atag = repo.lookup(tag.target_id)
-
-    if atag.is_a? Rugged::Tag::Annotation
-        puts "## #{atag.name}"
-        puts "*#{atag.tagger[:time]}*"
-        puts atag.message.split("\n").map{|l| "  #{l}"}.join("\n")
-        puts
+class RepoTag
+    def initialize(repo_name, tag)
+        @repo_name = repo_name
+        @tag = tag
     end
+
+    def time
+        @tag.tagger[:time]
+    end
+
+    def to_s
+        s = "## #{@repo_name}: #{@tag.name}\n"
+        s += "*#{self.time}*\n"
+        s += @tag.message.split("\n").map{|l| "  #{l}"}.join("\n")
+        return s
+    end
+end
+
+all_tags = ARGV.map { |arg|
+    repo_name = File.split(arg)[-1]
+    repo = Rugged::Repository.new(arg)
+
+    repo.tags.map { |tag|
+        atag = repo.lookup(tag.target_id)
+        atag.is_a?(Rugged::Tag::Annotation) ? RepoTag.new(repo_name, atag) : nil
+    }.find_all { |t| not t.nil? }
+}.flatten
+
+all_tags.sort_by { |t| t.time }.reverse.each do |tag|
+    puts tag
+    puts
 end
