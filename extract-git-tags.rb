c6fe0af3592e6cde4f0aef9222cbd1fd595e2ea6
David Poetzsch-Heffter <davidpoetzsch@web.de>
2017-02-04 13:14:32 +0100

    added script to extract tag content

---
+++ extract-git-tags.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require "rugged"
+
+if ARGV.length < 1
+    puts "Usage: extract-git-tags.rb <git-repo>"
+    exit 1
+end
+
+repo = Rugged::Repository.new(ARGV[0])
+
+repo.tags.sort_by{|t|t.name}.each do |tag|
+    atag = repo.lookup(tag.target_id)
+
+    if atag.is_a? Rugged::Tag::Annotation
+        puts "## #{atag.name}"
+        puts "*#{atag.tagger[:time]}*"
+        puts atag.message.split("\n").map{|l| "  #{l}"}.join("\n")
+        puts
+    end
+end
